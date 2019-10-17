c5e80e4a15dd07146e29c815b915bd7abd8e4a45
Parth-Bisag <parth.150180107051@gmail.com>
2019-10-17 18:03:31 +0530

    tut6

---
+++ tut6.js
@@ -0,0 +1,18 @@
+console.log('we are at tut 6');
+
+const name = 'dsfa';
+const name2 = 'mmm';
+console.log(name+' '+name2);
+
+
+
+//// mehtod of string concatination 
+
+let html;
+html = "<fsdfdsa>"+" "+"fdfadfasf";
+console.log(html);
+
+html = html.concat('this',' ','stre',' ','dfadsfa');
+console.log(html);
+
+
